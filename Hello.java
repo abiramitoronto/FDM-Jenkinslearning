b284ad1e894ba855a12d1a9ee15e52a979f59173
abiramitoronto <91011968+abiramitoronto@users.noreply.github.com>
2025-10-31 17:25:23 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class even {
+  public static void main(String args[]){
+
+  int num = 12;
+  if(num % 2 == 0){
+    System.out.println("EVEN num");
+  }
+  else{
+    System.out.println("odd num");
+  }
+  
+}
+}
